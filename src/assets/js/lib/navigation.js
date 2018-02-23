bb810459153929a42c88b2ab10bea56751a99efe
rath-prak <rathanap@gmail.com>
2018-02-23 21:06:44 +1300

    navigation

--- src/assets/js/lib/navigation.js
+++ src/assets/js/lib/navigation.js
@@ -1,27 +1,29 @@
 import $ from 'jquery';
 
 const navigation = () => {
-  let toggleBars = $(".toggle-bars");
-  let bar1 = toggleBars.find("b:nth-child(1)");
-  let bar2 = toggleBars.find("b:nth-child(2)");
-  let bar3 = toggleBars.find("b:nth-child(3)");
+  let toggleBarsWrapper = $(".toggle-bar-wrapper");
+  let navMenuText = toggleBarsWrapper.find("span");
+  let bar1 = toggleBarsWrapper.find("b:nth-child(1)");
+  let bar2 = toggleBarsWrapper.find("b:nth-child(2)");
+  let bar3 = toggleBarsWrapper.find("b:nth-child(3)");
   let overlayNavContent = $(".overlay-nav-content");
   let overlayNavContent2 = $(".overlay-nav-content-2");
 
     let tl = new TimelineMax({ paused:true });
 
-    tl.to(bar1, 0.3, { y: 15,})
+    tl.to(bar1, 0.3, { y: 10,})
       .to(bar1, 0.6, { rotation: -135, ease: Back.easeOut});
 
-    tl.to(bar3, 0.3, { y: -15,}, 0)
+    tl.to(bar3, 0.3, { y: -10,}, 0)
        .to(bar3, 0.6, { rotation: 135, ease: Back.easeOut}, "-=0.6");
 
     tl.to(bar2, 0.6, { x: 100, opacity: 0, ease: Back.easeIn}, -0.6);
+    tl.to(navMenuText, 0.6, { x: 150, opacity: 0, ease: Back.easeIn}, -0.6);
 
     tl.to(overlayNavContent, 1, { x: "50%", ease: Back.easeOut }, 0.5);
     tl.to(overlayNavContent2, 1, { x: "0", ease: Back.easeOut }, 0.5);
 
-  toggleBars.click(function(){
+  toggleBarsWrapper.click(function(){
     if(tl.reversed()){
        tl.play();
      }else{
