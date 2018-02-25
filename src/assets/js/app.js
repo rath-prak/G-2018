af453750e6d96965e06f1f30db0476bea0c57d5c
rath-prak <rathanap@gmail.com>
2018-02-26 04:00:10 +1300

    fix navigtion

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,6 +2,19 @@ import $ from 'jquery';
 import whatInput from 'what-input';
 import { TweenMax, Power2, TimelineLite } from "gsap";
 
+import slick from './lib/slick.min.js'
+
 import navigation from './lib/navigation';
 
 navigation();
+
+
+
+$(document).ready(function(){
+  // SLIDER
+  // $('.single-item-test').slick({
+  //   autoplay: true,
+  //   dots: true,
+  //   arrow: true,
+  // });
+});
