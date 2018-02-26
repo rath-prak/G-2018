7af08d178c61cf4728bef19a5d364f0beafd0da2
rath-prak <rathanap@gmail.com>
2018-02-26 15:39:18 +1300

    fix navigation

--- src/assets/js/lib/navigation.js
+++ src/assets/js/lib/navigation.js
@@ -8,6 +8,7 @@ const navigation = () => {
   let bar3 = toggleBarsWrapper.find("b:nth-child(3)");
   let overlayNavContent = $(".overlay-nav-content");
   let overlayNavContent2 = $(".overlay-nav-content-2");
+  let overlayBlock = $(".overlay-block");
 
     let tl = new TimelineMax({ paused:true });
 
@@ -20,8 +21,9 @@ const navigation = () => {
     tl.to(bar2, 0.6, { x: 100, opacity: 0, ease: Back.easeIn}, -0.6);
     tl.to(navMenuText, 0.6, { x: 150, opacity: 0, ease: Back.easeIn}, -0.6);
 
-    tl.to(overlayNavContent, 1, { x: "10%", ease: Back.easeOut }, 0.5);
+    tl.to(overlayNavContent, 1, { x: "0", ease: Back.easeOut }, 0.5);
     tl.to(overlayNavContent2, 1, { x: "0", ease: Back.easeOut }, 0.5);
+    tl.to(overlayBlock, 1, { x: "0", }, 0.5);
 
   toggleBarsWrapper.click(function(){
     if(tl.reversed()){
